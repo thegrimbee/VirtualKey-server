d2a883e0a16a6352780c60e689958045e92e3256
thegrimbee <gabriel.mario.antaputra@gmail.com>
2024-12-28 20:29:42 +0800

    added login and register route

--- server.py
+++ server.py
@@ -26,3 +26,21 @@ def generate_hash(request: ValidationRequest):
     truncated_hex_digest = hex_digest[:40]
     return {"username": request.username, "is_valid": hash_to_compare == truncated_hex_digest}
 
+@app.post("/register")
+def register_user(request: ValidationRequest):
+    if request.username in users:
+        raise HTTPException(status_code=400, detail="User already exists")
+    database["users"][request.username] = {"key": request.received_hash}
+    json.dump(database, open("database.json", "w"))
+    return {"username": request.username, "key": request.received_hash}
+
+@app.post("/login")
+def login_user(request: ValidationRequest):
+    if request.username not in users:
+        raise HTTPException(status_code=400, detail="User does not exist")
+    sha256_hash = hashlib.sha256()
+    input_string = request.password + SALT
+    sha256_hash.update(input_string.encode('utf-8'))
+    if sha256_hash.hexdigest() != users[request.username]["hash"]:
+        raise HTTPException(status_code=400, detail="Invalid password")
+    return {"username": request.username, "key": users[request.username]["key"]}
